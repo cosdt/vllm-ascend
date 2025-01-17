eef4f39520807ec11a52fff87e41b50b31cc91f6
hw_whx <wanghexiang7@huawei.com>
2025-01-17 11:20:19 +0800

    try to import mindie_turbo in register() of vllm_ascend.__init__
    Signed-off-by: hw_whx <wanghexiang7@huawei.com>

--- vllm_ascend/__init__.py
+++ vllm_ascend/__init__.py
@@ -1,8 +1,9 @@
 def register():
     """Register the NPU platform."""
     try:
-        from mindie_turbo import vllm_turbo
+        from mindie_turbo import vllm_turbo # noqa: F401
     except ImportError:
-        import logging
-        logging.info("MindIE Turbo is not installed. Running vllm without turbo.")
+        from vllm.logger import init_logger
+        logger = init_logger(__name__)
+        logger.info("MindIE Turbo is not installed. Running vllm without turbo.")
     return "vllm_ascend.platform.NPUPlatform"
