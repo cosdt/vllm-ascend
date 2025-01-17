03374b4726ac55d1d9914fe56ca88e612fb7ecba
MengqingCao <cmq0113@163.com>
2025-01-17 05:15:23 +0000

    code format
    Signed-off-by: MengqingCao <cmq0113@163.com>

--- vllm_ascend/__init__.py
+++ vllm_ascend/__init__.py
@@ -1,6 +1,11 @@
-# flake8: noqa: F401
 def register():
     """Register the NPU platform."""
+    register_mindie_turbo()
+    return "vllm_ascend.platform.NPUPlatform"
+
+
+# flake8: noqa: F401
+def register_mindie_turbo():
     try:
         from mindie_turbo import vllm_turbo  # type: ignore
     except ImportError:
@@ -8,4 +13,3 @@ def register():
         logger = init_logger(__name__)
         logger.info(
             "MindIE Turbo is not installed. Running vllm without turbo.")
-    return "vllm_ascend.platform.NPUPlatform"
